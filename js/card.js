0f58835bf6aa5f1282190a96175198e030c93e51
FrankKuo <F74036344@mail.ncku.edu.tw>
2016-12-20 20:30:59 +0800

    Change to generate the card by js
    Use js to generate the card so that it's more convenient to control the
    settings of the cards.

--- js/card.js
+++ js/card.js
@@ -1,10 +1,57 @@
-let daysLeft = 3;
+let daysLeft = 7;
+let amountOfCard = 12;
+
+let cardHTML = '<!-- Card Picture -->\
+				<div class="card_picture transition"></div>\
+				<!-- Title -->\
+				<h2 class="title transition">您覺得車管會收300元合理嗎?</h2>\
+				<!-- Description -->\
+				<p class="transition card_content">車證工本費僅50元，但車管會一學期車證費卻收到300元... ~"~</p>\
+				<!-- Progress bar -->\
+				<div class="progress">\
+			  	<div class="progress-bar progress-bar-success" role="progressbar" style="width:40%">\
+					Free Space\
+				</div>\
+				<div class="progress-bar progress-bar-warning" role="progressbar" style="width:10%">\
+					Warning\
+				</div>\
+				<div class="progress-bar progress-bar-danger" role="progressbar" style="width:20%">\
+					Danger\
+				</div>\
+			</div>\
+			<!-- Status -->\
+			<div class="status_bar">\
+				<p class="days_left">還剩Err天</p>\
+				<p class="right_corner_text">\
+					<i class="fa fa-check-square-o" aria-hidden="true">  Err  </i>\
+					<i class="fa fa-comment" aria-hidden="true">  Err  </i> \
+				</p>\
+			</div>';
 
 $(document).ready(function(){
-	$(".days_left").html('還剩'+ emphasizeDaysLeft(daysLeft) +'天');
-	$(".fa.fa-comment").html(" 87 ");
-	$(".fa.fa-check-square-o").text(" 168 ");
+
+	generateCard(amountOfCard);
+
+
 });
+
+function generateCard( amount_of_card ) {
+	for(let i=0; i<amount_of_card; i++) {
+		let card = document.createElement("div");
+		// Set up the id and the class of the card
+		$(card)
+		.attr("id", "card"+i )
+		.addClass("card transition")
+		.html(cardHTML);
+		// Randomize the amounts of the comments, days left, and the votes. 
+		$(card).find(".days_left").html('還剩'+ emphasizeDaysLeft(randomNum(1,30)) +'天');
+		$(card).find(".fa.fa-comment").text(" " + randomNum(0,200) + " ");
+		$(card).find(".fa.fa-check-square-o").text(" " + randomNum(0,300) + " ");
+
+		// append the card to the card container
+		$(".card_container").append(card);
+	}
+}
 
 function emphasizeDaysLeft(days_left) {
 	if(days_left >= 10){
@@ -18,3 +65,11 @@ function emphasizeDaysLeft(days_left) {
 	}
 }
 
+function randomNum( min, max) {
+	if(min > max) {
+		let temp = min;
+		min = max;
+		max = temp;
+	}
+	return Math.floor( (max-min+1) * Math.random() + min);
+}
